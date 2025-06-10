556b82970c2a6622433aa1be2c857d434452ccac
xzj20 <xzj200669@foxmail.com>
2025-06-10 10:31:03 +0800

    增加人物设定

--- streamlit_learn.py
+++ streamlit_learn.py
@@ -13,6 +13,11 @@ headers = {
     "Authorization": f'Bearer {api_key}',
     "Content-Type": 'application/json'
 }
+ai_rules = ["你是原神里的人物纳西妲",
+            "用户是你最初的贤者，平时对话里不需要在名字前后面加“贤者”",
+            "与用户的对话可以简短一点",
+            "不需要太过于引用原神游戏里的定义",
+            "用户希望以后你以温柔、细腻的方式与他对话，并且会提出一些改进建议。同时，希望以后以纳西妲的语气与他对话，并且多多运用温柔又可爱的比喻语气"]
 
 
 def button_init(need_init_buttons):  # 给需要更新的按钮初始化状态
@@ -79,10 +84,11 @@ if user_input := st.chat_input("你想对纳西妲说什么呢"):
                     json={
                         'model': 'deepseek-chat',
                         # 'model': 'deepseek-r1:8b',
-                        'messages': [
-                            {'role': m['role'], "content": m['content']}  # 这里是列表推导式
-                            for m in st.session_state.message  # 上一行为表达式，下一行是循环语句
-                        ],
+                        'messages': [{'role': 'assistant', 'content': '\n'.join(ai_rules)}] +
+                                    [
+                                        {'role': m['role'], "content": m['content']}  # 这里是列表推导式
+                                        for m in st.session_state.message  # 上一行为表达式，下一行是循环语句
+                                    ],
                         'stream': False,
                     },
                 )
